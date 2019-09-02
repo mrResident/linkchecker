1f45586da69b5e72d86216c5045f7a6355fb38e2
Alexander <alexandrov@resprojects.ru>
2019-09-03 00:31:26 +0300

    Restart project
    Now for this project will used spring boot

---
+++ src/test/java/ru/resprojects/linkchecker/LinkcheckerApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.resprojects.linkchecker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LinkcheckerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
